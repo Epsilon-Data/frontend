47f718c8b706a10ae6352e03c23f33a581e2f528
chuleeshen <leeshen.chu@monash.edu>
2025-06-03 17:20:55 +0800

    fix: removed unwanted words and constants

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -1,35 +1,17 @@
-import { ProjectInfo } from '@app/api/projects.api';
 import { AccessDetails } from '@app/interfaces/interfaces';
-import { t } from 'i18next';
 
 export const DATE_FORMAT = 'dd/MM/yyyy';
 export const PROJECT_API_URL = '/hub/project';
 
-export const INITIAL_DETAIL_VALUES: ProjectInfo = {
+export const INITIAL_DETAIL_VALUES = {
   name: '',
-  duration: [],
   lead: '',
   members: [],
   university: '',
   faculty: '',
   ethicsId: '',
   description: '',
-  dataDescription: '',
-  collectionDuration: [],
-  dataKeywords: [],
-  dataParticipantsNum: 0,
-  archetype: { id: '', name: '', nodes: [], edges: [] },
-  visualisations: [],
-  isOwnProject: false,
-  lastUpdated: '',
 };
-
-export const SEARCH_FIELDS = [
-  { value: 'all', label: t('browse.topSearch.fields.all') },
-  { value: 'name', label: t('browse.topSearch.fields.projectTitle') },
-  { value: 'keywords', label: t('browse.topSearch.fields.keywords') },
-  { value: 'organisation', label: t('browse.topSearch.fields.organisation') },
-];
 
 export const INITIAL_ACCESS_VALUES: AccessDetails = {
   id: '',
--- src/pages/BrowseDatasetPages/DatasetInfoPage/DatasetInfoPage.tsx
+++ src/pages/BrowseDatasetPages/DatasetInfoPage/DatasetInfoPage.tsx
@@ -1,23 +1,17 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { PageTitle } from '@app/components/common/PageTitle/PageTitle';
 import * as S from './DatasetInfoPage.styles';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { BaseRow } from '@app/components/common/BaseRow/BaseRow';
 import { BaseCol } from '@app/components/common/BaseCol/BaseCol';
-import { CgEnter } from 'react-icons/cg';
-import { Flex, Space, Tabs, Tag, Typography } from 'antd';
+import { Tabs, Typography } from 'antd';
 import { InfoSectionHeader } from '@app/components/display-info/InfoSectionHeader';
 import { InfoItem } from '@app/components/display-info/InfoItem';
 import { ProjectDetails } from './ProjectDetails/ProjectDetails';
-import ReactFlow, { ReactFlowProvider } from 'reactflow';
-import { PermissionNode } from '@app/components/reactflow-components/PermissionNode/PermissionNode';
 import 'reactflow/dist/style.css';
 import { useMounted } from '@app/hooks/useMounted';
 import { ProjectInfo, getProjectDetails } from '@app/api/projects.api';
-import { DATE_FORMAT, INITIAL_DETAIL_VALUES } from '@app/constants/projects';
-import { format } from 'date-fns';
-import { createNodeTypes, EDGE_TYPES, REACT_FLOW_OPTIONS } from '@app/constants/reactflow';
 
 declare global {
   interface Window {
@@ -26,18 +20,15 @@ declare global {
   }
 }
 
-const { tableau } = window;
-
 const DatasetSummaryPage: React.FC = () => {
   const { id } = useParams();
   const { t } = useTranslation();
   const { isMounted } = useMounted();
-  const navigate = useNavigate();
-  const [visList, setVisList] = useState<{ title: string; url: string }[]>([]);
-  const [projectDetails, setProjectDetails] = useState<ProjectInfo>(INITIAL_DETAIL_VALUES);
+  // const [visList, setVisList] = useState<{ title: string; url: string }[]>([]);
+  const [projectDetails, setProjectDetails] = useState<ProjectInfo>({} as ProjectInfo);
 
-  const nodeTypes = useMemo(() => createNodeTypes(PermissionNode), []);
-  const edgeTypes = useMemo(() => EDGE_TYPES, []);
+  // const nodeTypes = useMemo(() => createNodeTypes(PermissionNode), []);
+  // const edgeTypes = useMemo(() => EDGE_TYPES, []);
 
   const tabItems = [
     { label: t('browse.info.details.title'), children: <ProjectDetails info={projectDetails} />, key: 'details' },
@@ -48,27 +39,25 @@ const DatasetSummaryPage: React.FC = () => {
     getProjectDetails(id).then((res) => {
       if (isMounted.current) {
         setProjectDetails(res);
-        setVisList(res.visualisations);
       }
     });
   }, [isMounted, id]);
 
   useEffect(() => {
-    const urls = visList.map((item) => item.url);
-    const options = {
-      hideTabs: true,
-    };
-
-    for (let i = 0; i < urls.length; i++) {
-      const container = document.getElementById(`vis-${i}`);
-      const vizs = new tableau.VizManager.getVizs();
-      if (vizs.length > 0) {
-        for (let j = 0; j < vizs.length; j++) {
-          vizs[j].dispose();
-        }
-      }
-      new tableau.Viz(container, urls[i], options);
-    }
+    // const urls = visList.map((item) => item.url);
+    // const options = {
+    //   hideTabs: true,
+    // };
+    // for (let i = 0; i < urls.length; i++) {
+    //   const container = document.getElementById(`vis-${i}`);
+    //   const vizs = new tableau.VizManager.getVizs();
+    //   if (vizs.length > 0) {
+    //     for (let j = 0; j < vizs.length; j++) {
+    //       vizs[j].dispose();
+    //     }
+    //   }
+    //   new tableau.Viz(container, urls[i], options);
+    // }
   });
 
   return (
@@ -80,20 +69,8 @@ const DatasetSummaryPage: React.FC = () => {
             <BaseCol span={6}>
               <S.OrgLink>{projectDetails.university}</S.OrgLink>
               <S.Text style={{ marginTop: '0.1rem', color: 'var(--text-light-color)' }}>
-                {`Last Updated: ${projectDetails.lastUpdated}`}
+                {`Last Updated: ${projectDetails.endDate}`}
               </S.Text>
-            </BaseCol>
-            <BaseCol span={5} offset={13}>
-              {!projectDetails.isOwnProject && (
-                <S.RequestButton
-                  type="primary"
-                  key="request"
-                  icon={<CgEnter />}
-                  onClick={() => navigate(`/browse/access/${id}/dataset`)}
-                >
-                  {t('browse.info.requestAccess')}
-                </S.RequestButton>
-              )}
             </BaseCol>
           </BaseRow>
           <BaseRow style={{ marginTop: '2rem', display: 'flex', flexDirection: 'column' }}>
@@ -113,48 +90,18 @@ const DatasetSummaryPage: React.FC = () => {
               <S.Title>{t('browse.info.aboutDataset')}</S.Title>
             </S.InfoHeader>
             <InfoSectionHeader text={t('browse.info.description')} />
-            <S.Text>{projectDetails.dataDescription}</S.Text>
-            {projectDetails.collectionDuration.length > 0 && (
-              <InfoItem
-                label={t('browse.info.collectionDuration')}
-                text={`${format(projectDetails.collectionDuration[0], DATE_FORMAT)} - ${format(
-                  projectDetails.collectionDuration[1],
-                  DATE_FORMAT,
-                )}`}
-              />
-            )}
-            <InfoItem label={t('browse.info.participantsNum')} text={projectDetails.dataParticipantsNum.toString()} />
+            <S.Text>{projectDetails.description}</S.Text>
+            <InfoItem label={t('browse.info.participantsNum')} text={projectDetails.dbParticipantsNum.toString()} />
             <div style={{ display: 'flex', flexDirection: 'row' }}>
               <Typography.Text strong>{t('browse.info.tags') + ': '}</Typography.Text>
-              <Flex gap="4px 3px" wrap="wrap" style={{ marginLeft: '0.5rem' }}>
-                {projectDetails.dataKeywords.map((tag) => (
-                  <Tag key={tag} color="blue">
-                    {tag}
-                  </Tag>
-                ))}
-              </Flex>
             </div>
-
-            {visList.length > 0 && (
-              <>
-                <InfoSectionHeader text={t('browse.info.visualisations')} />
-                <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
-                  {visList.map((vis, index) => (
-                    <div key={`vis-${index}`}>
-                      <S.Text>{vis.title}</S.Text>
-                      <div id={`vis-${index}`} style={{ height: '20%' }}></div>
-                    </div>
-                  ))}
-                </Space>
-              </>
-            )}
           </BaseRow>
           <S.HorizontalDivider />
           <BaseRow style={{ marginTop: '2rem', display: 'flex', flexDirection: 'column' }}>
             <S.InfoHeader>
               <S.Title>{t('browse.info.datasetArchetype')}</S.Title>
             </S.InfoHeader>
-            {projectDetails.archetype ? (
+            {/* {projectDetails.archetype ? (
               <ReactFlowProvider>
                 <S.MapWrapper>
                   <ReactFlow
@@ -180,7 +127,7 @@ const DatasetSummaryPage: React.FC = () => {
               </ReactFlowProvider>
             ) : (
               <S.Text style={{ marginTop: '1rem' }}>{t('browse.info.noArchetype')}</S.Text>
-            )}
+            )} */}
           </BaseRow>
         </S.Card>
       </S.CardWrapper>

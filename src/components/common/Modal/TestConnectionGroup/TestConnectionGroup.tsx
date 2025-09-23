c86c1ece6aa7a1b5b4b31380963f58ab103502b3
chuleeshen <leeshen.chu@monash.edu>
2025-09-23 18:24:30 +0800

    style: adjust styles to match design

--- src/components/common/Modal/TestConnectionGroup/TestConnectionGroup.tsx
+++ src/components/common/Modal/TestConnectionGroup/TestConnectionGroup.tsx
@@ -31,6 +31,38 @@ export const TestConnectionGroup: React.FC<{
   return (
     <div className="flex flex-col pb-12">
       <NumberedFormItem number={4}>
+        <InputLabel
+          inputTitle={t('dashboard.createProject.form.step3.dbCred.title')}
+          inputDescription={t('dashboard.createProject.form.step3.dbCred.description')}
+        />
+        <div className="flex-1">
+          <p className="mb-1 text-xs">{t('dashboard.createProject.form.step3.dbCred.username.title')}</p>
+          <FormItem name="username" rules={inputRules}>
+            <Input
+              className="border border-black bg-grey-4"
+              placeholder={t('dashboard.createProject.form.step3.dbCred.username.placeholder')}
+            />
+          </FormItem>
+        </div>
+        <div className="flex-1 mb-12">
+          <p className="mb-1 text-xs">{t('dashboard.createProject.form.step3.dbCred.password.title')}</p>
+          <FormItem
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: t('fieldMessages.input.required'),
+              },
+            ]}
+          >
+            <Input.Password
+              className="border border-black bg-grey-4"
+              placeholder={t('dashboard.createProject.form.step3.dbCred.password.placeholder')}
+            />
+          </FormItem>
+        </div>
+      </NumberedFormItem>
+      <NumberedFormItem number={5} showDivider={false}>
         <InputLabel inputTitle={inputTitle} inputDescription={inputDescription} />
         <Row className="flex justify-between">
           <Col span={18}>
@@ -54,50 +86,18 @@ export const TestConnectionGroup: React.FC<{
             </Button>
           </Col>
         </Row>
-        <div>
+        <div className="mt-1">
           {connected ? (
-            <div className="text-xs text-success flex leading-5">
+            <div className="text-sm text-success flex">
               {t('dashboard.createProject.form.step3.testConnection.success')}
               <FaRegCircleCheck className="ml-2 mt-1" />
             </div>
           ) : show ? (
-            <div className="text-xs text-error flex leading-5">
+            <div className="text-sm text-error flex">
               {t('dashboard.createProject.form.step3.testConnection.failed')}
               <PiWarningBold className="ml-2 mt-1" />
             </div>
           ) : null}
-        </div>
-      </NumberedFormItem>
-      <NumberedFormItem number={5} showDivider={false}>
-        <InputLabel
-          inputTitle={t('dashboard.createProject.form.step3.dbCred.title')}
-          inputDescription={t('dashboard.createProject.form.step3.dbCred.description')}
-        />
-        <div className="flex-1">
-          <p className="mb-1 text-xs">{t('dashboard.createProject.form.step3.dbCred.username.title')}</p>
-          <FormItem name="username" rules={inputRules}>
-            <Input
-              className="border border-black bg-grey-4"
-              placeholder={t('dashboard.createProject.form.step3.dbCred.username.placeholder')}
-            />
-          </FormItem>
-        </div>
-        <div className="flex-1">
-          <p className="mb-1 text-xs">{t('dashboard.createProject.form.step3.dbCred.password.title')}</p>
-          <FormItem
-            name="password"
-            rules={[
-              {
-                required: true,
-                message: t('fieldMessages.input.required'),
-              },
-            ]}
-          >
-            <Input.Password
-              className="border border-black bg-grey-4"
-              placeholder={t('dashboard.createProject.form.step3.dbCred.password.placeholder')}
-            />
-          </FormItem>
         </div>
       </NumberedFormItem>
     </div>

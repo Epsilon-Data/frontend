63b08bb51bad9ef9be55ee1e870926aa40d7b13f
chuleeshen <leeshen.chu@monash.edu>
2025-09-26 17:49:29 +0800

    refactor: remove node toolbar for subcategory node

--- src/components/reactflow-components/SubcategoryNode/SubcategoryNode.tsx
+++ src/components/reactflow-components/SubcategoryNode/SubcategoryNode.tsx
@@ -1,63 +1,10 @@
-import { Position, NodeProps, NodeToolbar } from 'reactflow';
-import { useState } from 'react';
+import { Position, NodeProps } from 'reactflow';
 import * as S from './SubcategoryNode.styles';
-import { SearchOutlined } from '@ant-design/icons';
-import { useTranslation } from 'react-i18next';
 import { NodeLabelData } from '@app/constants/reactflow/types';
 
-export interface SubcatNodeData extends NodeLabelData {
-  availableColumns?: string[];
-}
-
-export function SubcategoryNode({ data, selected }: NodeProps<SubcatNodeData>) {
-  const [showColumnMenu, setShowColumnMenu] = useState(false);
-  const [searchValue, setSearchValue] = useState('');
-  const [filteredColumns, setFilteredColumns] = useState<string[]>(data.availableColumns || []);
-  const { t } = useTranslation();
-
-  const handleClick = () => {
-    if (showColumnMenu) {
-      setShowColumnMenu(false);
-    } else {
-      if (data.availableColumns && data.availableColumns.length > 0) {
-        setShowColumnMenu(true);
-      }
-    }
-  };
-
-  const handleColumnSelect = (columnName: string) => {
-    console.log('Selected column:', columnName);
-    setShowColumnMenu(false);
-  };
-
-  const handleSearchChange = (event: { target: { value: string } }) => {
-    setSearchValue(event.target.value);
-    const filteredColumns = data.availableColumns?.filter((column) =>
-      column.toLowerCase().includes(event.target.value.toLowerCase()),
-    );
-    setFilteredColumns(filteredColumns || []);
-  };
-
+export function SubcategoryNode({ data, selected }: NodeProps<NodeLabelData>) {
   return (
-    <S.SubcategoryNodeWrapper className="subcategory-node" onClick={handleClick} $selected={selected}>
-      <NodeToolbar position={Position.Right}>
-        <S.ColumnSelectMenu>
-          <S.ColumnSearch
-            prefix={<SearchOutlined rev={undefined} style={{ marginRight: '0.5rem' }} />}
-            placeholder={t('databaseSources.describeDataset.columnSidebar.search')}
-            onChange={handleSearchChange}
-            value={searchValue}
-            allowClear
-          />
-          {filteredColumns.map((columnName, index) => (
-            <S.Column key={index} onClick={() => handleColumnSelect(columnName)}>
-              <div className="text">
-                <span>{columnName}</span>
-              </div>
-            </S.Column>
-          ))}
-        </S.ColumnSelectMenu>
-      </NodeToolbar>
+    <S.SubcategoryNodeWrapper className="subcategory-node" $selected={selected}>
       <S.SubcategoryDisplay>{data.label}</S.SubcategoryDisplay>
       <S.SubcategoryHandle type="target" position={Position.Bottom} />
     </S.SubcategoryNodeWrapper>

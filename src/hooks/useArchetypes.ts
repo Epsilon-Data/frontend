eb85b4ecd5bd2e816b6f92e206b199dc020d6c6d
chuleeshen <leeshen.chu@monash.edu>
2025-10-28 17:30:34 +0800

    feat: add fetching for single archetype

--- src/hooks/useArchetypes.ts
+++ src/hooks/useArchetypes.ts
@@ -1,21 +1,38 @@
-import { Archetype, getArchetypes } from '@app/api/archetypes.api';
+import { Archetype, ArchetypeInfo, getArchetypeDetails, getArchetypes } from '@app/api/archetypes.api';
 import { useCallback, useState } from 'react';
 
 export const useArchetypes = (projectId: string) => {
+  const [archetype, setArchetype] = useState<ArchetypeInfo>({} as ArchetypeInfo);
   const [archetypes, setArchetypes] = useState<Archetype[]>([]);
-  const [loading, setLoading] = useState<boolean>(false);
+  const [tableLoading, setTableLoading] = useState<boolean>(false);
+  const [manageLoading, setManageLoading] = useState<boolean>(false);
 
   const fetchArchetypes = useCallback(async () => {
-    setLoading(true);
+    setTableLoading(true);
     try {
       const userArchetypes = await getArchetypes(projectId);
       setArchetypes(userArchetypes);
     } catch (error) {
       console.error('Failed to fetch archetypes for project:', error);
     } finally {
-      setLoading(false);
+      setTableLoading(false);
     }
   }, [projectId]);
 
-  return { archetypes, loading, fetchArchetypes };
+  const fetchArchetype = useCallback(
+    async (archetypeId: string) => {
+      setManageLoading(true);
+      try {
+        const selectedArchetype = await getArchetypeDetails(projectId, archetypeId);
+        setArchetype(selectedArchetype);
+      } catch (error) {
+        console.error('Failed to fetch archetype for project:', error);
+      } finally {
+        setManageLoading(false);
+      }
+    },
+    [projectId],
+  );
+
+  return { archetypes, archetype, tableLoading, manageLoading, fetchArchetypes, fetchArchetype };
 };

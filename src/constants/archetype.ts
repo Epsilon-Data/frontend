6517c05a891bf647629e4b2c48184733a99ad25e
chuleeshen <leeshen.chu@monash.edu>
2025-10-28 17:29:46 +0800

    refactor: add constant for getting status tag colour

--- src/constants/archetype.ts
+++ src/constants/archetype.ts
@@ -1,6 +1,12 @@
 export const ARCHETYPE_API_URL = '/hub/archetype';
 export const DATE_FORMAT = 'DD/MM/YYYY';
 
+export const STATUS_COLORS: { [key: string]: string } = {
+  PUBLISHED: '#1481F1',
+  ACTIVE: '#50C878',
+  DRAFT: '#AEAEAE',
+};
+
 export function toTitleCase(text: string) {
   return text
     .toLowerCase()

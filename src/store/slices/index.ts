d46217b48f907eac98d202a967654a473ef47851
chuleeshen <leeshen.chu@monash.edu>
2025-10-28 17:17:00 +0800

    chore: remove redundant project slice

--- src/store/slices/index.ts
+++ src/store/slices/index.ts
@@ -3,7 +3,6 @@ import authReducer from '@app/store/slices/authSlice';
 import nightModeReducer from '@app/store/slices/nightModeSlice';
 import themeReducer from '@app/store/slices/themeSlice';
 import pwaReducer from '@app/store/slices/pwaSlice';
-import projectReducer from '@app/store/slices/projectSlice';
 
 export default {
   user: userReducer,
@@ -11,5 +10,4 @@ export default {
   nightMode: nightModeReducer,
   theme: themeReducer,
   pwa: pwaReducer,
-  project: projectReducer,
 };

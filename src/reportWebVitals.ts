d0ad5dc25694ae86dc58a3056b4cc28bf9360dbd
chuleeshen <leeshen.chu@monash.edu>
2025-12-04 16:12:40 +0800

    feat(deps): update minor packages and affected code

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,16 +1,17 @@
-import { ReportHandler } from 'web-vitals';
+import type { Metric } from 'web-vitals';
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-  if (onPerfEntry) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
+type ReportHandler = (metric: Metric) => void;
+
+const reportWebVitals = (onPerfEntry?: ReportHandler): void => {
+  if (!onPerfEntry) return;
+
+  import('web-vitals').then((vitals) => {
+    vitals.onCLS(onPerfEntry);
+    vitals.onINP(onPerfEntry);
+    vitals.onFCP(onPerfEntry);
+    vitals.onLCP(onPerfEntry);
+    vitals.onTTFB(onPerfEntry);
+  });
 };
 
 export default reportWebVitals;

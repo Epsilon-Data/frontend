d63fbc81cc9654b996a0128061931ead53f1f718
Vincent <vincent.wesley.liem@gmail.com>
2026-01-05 10:43:34 +0800

    refactor: modularize the archetype creation to be reusable in different cypress test files

--- cypress/e2e/archetypes/create-archetype.cy.ts
+++ cypress/e2e/archetypes/create-archetype.cy.ts
@@ -6,34 +6,12 @@ describe('Create Archetype', () => {
   });
 
   it('should navigate to a test project and create archetype', () => {
-    // find any project card that has "Ready" or "Mapped" status tag
-    cy.get('.ant-card')
-      .filter(':has(.ant-tag)')
-      .filter((index, card) => {
-        const tagText = Cypress.$(card).find('.ant-tag').text();
-        return tagText.includes('Ready') || tagText.includes('Mapped');
-      })
-      .first() // select the first matching card because i think it doesnt matter, as long as its ready or mapped
-      .as('testProjectCard'); // alias it for later use
+    cy.archetypeViewReadyOrMappedProjectCard();
 
-    // view project and create new template
-    cy.get('@testProjectCard').trigger('mouseover');
-    cy.get('@testProjectCard').find('button').contains('View project').click();
-    cy.contains('button', 'New template').click();
-
-    cy.get('.ant-modal input[type="text"]').type('Test Archetype Template CYPRESS');
+    cy.archetypeFillName('Test Archetype Template CYPRESS');
     cy.contains('button', 'Next').click();
 
-    // create a new react flow node
-    cy.contains('.react-flow__node', 'Main Entity')
-      .click()
-      .within(() => {
-        cy.get('.react-flow__handle')
-          .should('exist')
-          .trigger('mousedown', { button: 0 })
-          .trigger('mousemove', { clientX: 100, clientY: 50 }) // Adjust coordinates as needed
-          .trigger('mouseup', { force: true });
-      });
+    cy.archetypeCreateNode();
     cy.contains('button', 'Next').click();
 
     cy.contains('.react-flow__node', 'Category 1').click();
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -29,6 +29,10 @@
 declare namespace Cypress {
   interface Chainable<Subject> {
     login(...options: any): Chainable<JQuery<HTMLElement>>;
+    archetypeViewReadyOrMappedProjectCard(...options: any): Chainable<JQuery<HTMLElement>>;
+    archetypeFillName(title: string, ...options: any): Chainable<JQuery<HTMLElement>>;
+    archetypeCreateNode(...options: any): Chainable<JQuery<HTMLElement>>;
+    archetypeMapNodeToColumn(...options: any): Chainable<JQuery<HTMLElement>>;
   }
 }
 
@@ -55,4 +59,55 @@ Cypress.Commands.add('login', () => {
 
     cy.url().should('not.include', 'keycloak');
   });
+});
+
+/**
+ * view a project card that is either READY or MAPPED
+ */
+Cypress.Commands.add('archetypeViewReadyOrMappedProjectCard', () => {
+  // find any project card that has "Ready" or "Mapped" status tag
+  cy.get('.ant-card')
+    .filter(':has(.ant-tag)')
+    .filter((index, card) => {
+      const tagText = Cypress.$(card).find('.ant-tag').text();
+      return tagText.includes('Ready') || tagText.includes('Mapped');
+    })
+    .first() // select the first matching card because i think it doesnt matter, as long as its ready or mapped
+    .as('testProjectCard'); // alias it for later use
+
+  // view project and create new template
+  cy.get('@testProjectCard').trigger('mouseover');
+  cy.get('@testProjectCard').find('button').contains('View project').click();
+  cy.contains('button', 'New template').click();
+});
+
+/**
+ * fills in the name of the archetype
+ */
+Cypress.Commands.add('archetypeFillName', (title: string) => {
+  cy.get('.ant-modal input[type="text"]').type(title);
+});
+
+/**
+ * creates a new node in the react flow archetype editor
+ */
+Cypress.Commands.add('archetypeCreateNode', () => {
+  cy.contains('.react-flow__node', 'Main Entity')
+    .click()
+    .within(() => {
+      cy.get('.react-flow__handle')
+        .should('exist')
+        .trigger('mousedown', { button: 0 })
+        .trigger('mousemove', { clientX: 100, clientY: 50 })
+        .trigger('mouseup', { force: true });
+    });
+});
+
+/**
+ * maps the default "Category 1" node to a column
+ */
+Cypress.Commands.add('archetypeMapNodeToColumn', () => {
+  cy.contains('.react-flow__node', 'Category 1').click();
+
+  cy.get('button.mt-2.w-full.text-left.rounded-md.border.border-sky-300.hover\\:bg-sky-50').first().click();
 });
